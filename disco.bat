e0c8aa799656d30d530d397d9da767518418ce46
saurav529 <saurav.bhandari@yahoo.com>
2019-05-16 19:46:21 +0545

    DISCO

---
+++ disco.bat
@@ -0,0 +1,22 @@
+@echo off
+
+:loop
+
+color 0a
+color 12
+color 3c
+color c3
+color 4d
+color 0b
+color 17
+color 0b
+color b6
+color a9
+color a7
+color 4a
+color 0b
+color 78
+color 90
+
+
+goto loop
